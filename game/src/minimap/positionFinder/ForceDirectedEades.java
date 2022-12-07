27bc480358d1770d08849d93045439e5a6446b69
AHeinisch <aheinisch@fh-bielefeld.de>
2022-12-07 08:11:56 +0100

    simple Eades demo class

--- game/src/minimap/positionFinder/ForceDirectedEades.java
+++ game/src/minimap/positionFinder/ForceDirectedEades.java
@@ -1,2 +1,19 @@
-package minimap.positionFinder;public class ForceDirectedEades {
+package minimap.positionFinder;
+
+import tools.Point;
+
+public class ForceDirectedEades<T> extends ForceDirected<T>{
+    float c_rep = 2;
+    float c_spring = 1;
+    float l = 1;
+
+    @Override
+    public Point attract(Point u, Point v) {
+        return null;
+    }
+
+    @Override
+    public Point repulsion(Point u, Point v) {
+        return null;
+    }
 }
